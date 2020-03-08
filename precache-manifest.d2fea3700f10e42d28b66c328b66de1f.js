5bc7e8f1d1fc6cf45d3a48e6233d089e46f5a026
DafaZakhulhaq27 <dafa27890@gmail.com>
2020-03-08 22:40:06 +0700

    Updates

---
+++ precache-manifest.d2fea3700f10e42d28b66c328b66de1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96da2c2a274382eea8a3204d6a5f1c9c",
+    "url": "/robot_react/index.html"
+  },
+  {
+    "revision": "dca2584c1fb03d2bf054",
+    "url": "/robot_react/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "57a1241d077ecf665e28",
+    "url": "/robot_react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dca2584c1fb03d2bf054",
+    "url": "/robot_react/static/js/2.b590f0d9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robot_react/static/js/2.b590f0d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57a1241d077ecf665e28",
+    "url": "/robot_react/static/js/main.eb78258c.chunk.js"
+  },
+  {
+    "revision": "6a26b711cdb00922fa53",
+    "url": "/robot_react/static/js/runtime-main.9e3dfdf9.js"
+  }
+]);
