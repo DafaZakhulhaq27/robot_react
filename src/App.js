bbe34d9ef779adfe6a7061912e81a164f476753d
DafaZakhulhaq27 <dafa27890@gmail.com>
2020-03-08 10:09:12 +0700

    add fetch and loading

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
+// import {robots} from './robots';
 
 class App extends Component {
   constructor(){
     super() 
     this.state = {
-      robots : robots,
+      robots : [],
       searchfield : ''  
     }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {this.setState({robots : users})})
   }
   onSearchChange = (event) =>{
     this.setState({searchfield : event.target.value})
@@ -19,14 +24,17 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robots => {
       return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
-    console.log(filteredRobots)    
-    return(
-      <div className='tc'>
-        <h1>Robot React</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots} />
-      </div>
-    );  
+    if(this.state.robots.length === 0){
+      return <h1>Loading</h1>
+    }else{
+      return(
+        <div className='tc'>
+          <h1>Robot React</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <CardList robots={filteredRobots} />
+        </div>
+      );    
+    }
   }
 }
 
